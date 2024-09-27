c261476559177d1c14038e7d95e1a280d1b08dfc
kevin0418 <54438552+kevin0418@users.noreply.github.com>
2024-09-27 23:38:18 +1000

    add prawnfarm menu
    add prawnfarm menu

---
+++ prawnadmenu.py
@@ -0,0 +1,61 @@
+import streamlit as st
+
+st.write("## Prawn Farm :red[Sale] ")  
+st.write ("### in Mullett Creek") 
+
+
+st.markdown(
+"""
+    
+    :blue[Prime Investment Opportunity] 
+    Welcome to an exceptional investment opportunity, more than 10% yueld
+
+    :blue[safe place from diseases]
+    it is restricted to the general public and far from general residential areas
+    Japanese company harvested about 150 tons per year for 10 years from 1993
+
+    :blue[Secure Income Stream]
+    It is produced about 10-12 tons of prawn per hectore.
+    Production is mainly Costco (70%) and fish market (30%) sales
+
+    :blue[Future Development]
+    The total area is 142 ha, and the aquaculture license area is 50 ha 
+    and 70 ha is expected.
+    Currently, 25 ha is being produced and gradually expanded,
+
+
+"""
+)     
+
+st.write("### Contact information ")
+st.write("**Yong Real Estate**")
+
+st.markdown(
+"""    
+  
+     Melanie Zhong  : 0450 634 279\n
+     Kevin OH : 0402 283 003
+"""
+)  
+with st.sidebar:
+     
+     
+     st.markdown(
+      """ 
+          **More Information**
+          
+      
+           - [Info.Memorandom (IM)](https://kevinoh-prawnfarmim.streamlit.app/)        
+    
+           - [Watch to video ](https://youtu.be/EZ4SLXaQLx0)
+    
+           - [Brochure ](https://kevinoh-prawnfarmbrochure.streamlit.app/)
+          
+                 
+    """
+    )
+     
+
+    
+
+
