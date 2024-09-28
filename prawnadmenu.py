748439a22fba1221c4ef36b5afe71d77ac98d978
kevin0418 <54438552+kevin0418@users.noreply.github.com>
2024-09-28 19:47:08 +1000

    Update mark 1
    Update mar st  1

--- prawnadmenu.py
+++ prawnadmenu.py
@@ -1,33 +1,36 @@
 import streamlit as st
 
-st.write("## Prawn Farm :red[Sale] ")  
-st.write ("### in Mullett Creek") 
+#st.write("## Prawn Farm :red[Sale] ")  
+#st.write ("### in Mullett Creek") 
+st.write("## Prawn Farm :red[Sale] ") 
+st.write("**in Mullett Creek**")  
+#st.write ("### in Mullett Creek") 
 
 
 st.markdown(
 """
     
-    :blue[Prime Investment Opportunity] 
-    Welcome to an exceptional investment opportunity, more than 10% yueld
+    :blue[Prime Investment Opportunity]\n 
+    Welcome to an exceptional investment opportunity, more than 10% yield
 
-    :blue[safe place from diseases]
+    :blue[safe place from diseases]\n
     it is restricted to the general public and far from general residential areas
     Japanese company harvested about 150 tons per year for 10 years from 1993
 
-    :blue[Secure Income Stream]
-    It is produced about 10-12 tons of prawn per hectore.
+    :blue[Secure Income Stream]\n
+    It is produced about 10-12 tons of prawn per hector.
     Production is mainly Costco (70%) and fish market (30%) sales
 
-    :blue[Future Development]
-    The total area is 142 ha, and the aquaculture license area is 50 ha 
-    and 70 ha is expected.
-    Currently, 25 ha is being produced and gradually expanded,
+    :blue[Future Development]\n
+    The total 142 ha, the aquaculture licensed is 50 ha and 70 ha is expected.
+    Currently 25 ha is being produced and gradually expanded
+    Over 100 approved species and  being tested for profitability.
 
 
 """
 )     
 
-st.write("### Contact information ")
+st.write("### Contact information")
 st.write("**Yong Real Estate**")
 
 st.markdown(
